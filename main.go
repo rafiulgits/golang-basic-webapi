51deddfc6d419381c1bf804d5ea4599f939776c1
Rafiul Islam <avoidcloud@gmail.com>
2020-12-24 00:26:30 +0600

    created basic http server with endpoint handlers to handle incoming requests

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "net/http"
 
+func main() {
+	//inline handler function
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(200)
+		w.Header().Add("Content-Type", "application/json")
+		w.Write([]byte(`{"message" : "hello world"}`))
+	})
+
+	//passing handler function
+	http.HandleFunc("/greet", worldHandler)
+
+	// :8090 meaning serve all hosts available in local machine
+	// or simply 0.0.0.0:8090
+	http.ListenAndServe(":8090", nil)
+}
+
+func worldHandler(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(200)
+	w.Header().Add("Content-Type", "application/json")
+	w.Write([]byte(`{"message" : "greet"}`))
 }
